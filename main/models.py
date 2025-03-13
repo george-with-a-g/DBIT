36970c2fcec8d482db9f5242b32555b1b84a3eae
george-with-a-g <apsgeorge6@gmail.com>
2025-03-13 18:50:24 +0300

    Adding topic and course models that can be linked to a question

--- main/models.py
+++ main/models.py
@@ -1,8 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Course(models.Model):
+    name = models.CharField(max_length=400, null=True, blank=True)
+    def __str__(self):
+        return f"{self.name}"
+
+class Topic(models.Model):
+    name = models.CharField(max_length=400, null=True, blank=True)
+    def __str__(self):
+        return f"{self.name}"
+
 class Question(models.Model):
     question = models.CharField(max_length=400, null=True, blank=True)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, null=True, blank=True)
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE, null=True, blank=True)
     def __str__(self):
         return f"{self.question}"
 
