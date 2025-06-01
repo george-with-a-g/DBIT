2052fa35ce6660f91e4fb441a3c82fd926cf0e0b
george-with-a-g <apsgeorge6@gmail.com>
2025-06-02 01:50:02 +0300

    Added authentication Page Template.

--- main/urls.py
+++ main/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='main'),
+    path('auth/', views.auth, name='auth'),
 ]
--- main/views.py
+++ main/views.py
@@ -9,3 +9,17 @@ def index(request):
     }
     return render(request, 'main/index.html', context)
 
+def auth(request):
+    if request.method == "POST":
+        signup_form_firstname = request.POST.get('Sign-Up-Form-FirstName')
+        signup_form_surname = request.POST.get('Sign-Up-Form-SurName')
+        signup_form_lastname = request.POST.get('Sign-Up-Form-LastName')
+        signup_form_email = request.POST.get('Sign-up-Form-4-Email')
+        signup_form_password = request.POST.get('Sign-up-Form-4-Password')
+
+        login_form_email = request.POST.get('Log-in-Form-4-Email')
+        login_form_password = request.POST.get('Log-in-Form-4-Password')
+
+
+    return render(request, 'main/authentication/index.html')
+
